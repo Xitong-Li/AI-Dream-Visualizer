628e7f88ee691b2a6733c26c8d073db5b8ae8af0
Xitong-Li <lneuxhak@126.com>
2025-06-11 03:46:55 +0800

    Delete AI-Dream-Visualizer directory

--- AI-Dream-Visualizer/ai_pipeline/model_loader.py
+++
@@ -1,8 +0,0 @@
-from diffusers import StableDiffusionPipeline
-from config import MODEL_ID, DEVICE
-import streamlit as st
-
-@st.cache_resource
-def load_sd_model():
-    pipe = StableDiffusionPipeline.from_pretrained(MODEL_ID).to(DEVICE)
-    return pipe
